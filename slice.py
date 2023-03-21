928ae483918a72c1597a0149b576cae69122055b
infiniteant <infinite021227@163.com>
2023-03-21 20:54:54 +0800

    slice

--- slice.py
+++ slice.py
@@ -95,3 +95,11 @@ fruits = ['grape', 'raspberry', 'apple', 'banana']
 print(sorted(fruits))
 
 print(fruits)
+
+print(sorted(fruits, reverse=True))
+print(sorted(fruits, key=len))
+
+print(sorted(fruits, key=len, reverse=True))
+print(fruits)
+fruits.sort()
+print(fruits)
